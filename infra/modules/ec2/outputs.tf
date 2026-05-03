e80a75828b91b61e958ed478f6cde569dd68f7ae
Vadivel M <vadivel.m@collinsongroup.com>
2026-05-03 21:51:26 +0530

    fix: bastion-host output

--- infra/modules/ec2/outputs.tf
+++ infra/modules/ec2/outputs.tf
@@ -1,3 +1,11 @@
 output "public_ip" {
   value = aws_instance.bastion.public_ip
 }
+
+output "public_dns" {
+  value = aws_instance.bastion.public_dns
+}
+
+output "instance_id" {
+  value = aws_instance.bastion.id
+}
