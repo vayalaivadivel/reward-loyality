33db9232b16afd3ff2e13575421903acb1286057
Vadivel M <vadivel.m@collinsongroup.com>
2026-05-03 21:57:13 +0530

    fix: bastion-host output issue fixed

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "rds_endpoint" {
   value = module.rds.db_endpoint
 }
+
+output "bastion_public_ip" {
+  value = module.bastion.public_ip
+}
+
+output "bastion_public_dns" {
+  value = module.bastion.public_dns
+}
