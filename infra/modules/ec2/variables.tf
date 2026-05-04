1f6d6f7fb0d3343f7822767dce8e4d6039ed321a
Vadivel M <vadivel.m@collinsongroup.com>
2026-05-04 11:24:15 +0530

    Duplicate variables

--- infra/modules/ec2/variables.tf
+++ infra/modules/ec2/variables.tf
@@ -1,24 +1,57 @@
-variable "ami" {}
-variable "public_subnet_id" {}
-variable "key_name" {}
-variable "name" {}
+variable "name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID for EC2"
+  type        = string
+}
+
+variable "public_subnet_id" {
+  description = "Public subnet where EC2 will be deployed"
+  type        = string
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
 variable "vpc_id" {
   description = "VPC ID for security group"
   type        = string
 }
 
-variable "rds_endpoint" {}
-variable "db_username" {}
-variable "db_password" {}
-variable "db_name" {}
 variable "project" {
-  type = string
+  description = "Project name"
+  type        = string
 }
 
 variable "env" {
-  type = string
+  description = "Environment (dev/stage/prod)"
+  type        = string
 }
 
-variable "vpc_id" {
-  type = string
+# -------- DB VARIABLES --------
+
+variable "rds_endpoint" {
+  description = "RDS endpoint"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
 }
