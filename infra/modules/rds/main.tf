38ef4d90a7222f2a3b3251f8303a81285078d1ae
Vadivel M <vadivel.m@collinsongroup.com>
2026-05-09 14:36:13 +0530

    Duplicate resource fixed

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -1,48 +1,3 @@
-resource "aws_db_subnet_group" "this" {
-  name       = "${var.name}-subnet"
-  subnet_ids = var.private_subnets
-
-  tags = {
-    Name = "${var.name}-subnet"
-  }
-}
-
-resource "aws_security_group" "rds" {
-  name   = "${var.name}-rds-sg"
-  vpc_id = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_db_instance" "mysql" {
-  identifier = var.name
-  db_name    = var.db_name
-
-  engine            = "mysql"
-  instance_class    = "db.t3.micro"
-  allocated_storage = 20
-
-  username = var.username
-  password = var.password
-
-  skip_final_snapshot = true
-
-
-  db_subnet_group_name   = aws_db_subnet_group.this.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
-
-  timeouts {
-    create = "30m"
-    delete = "30m"
-  }
-}
-
-
 #########################################
 # RDS SUBNET GROUP
 #########################################
@@ -123,7 +78,7 @@ resource "aws_db_parameter_group" "mysql_cdc_pg" {
   #################################
 
   parameter {
-    name  = "binlog retention hours"
+    name  = "binlog_retention_hours"
     value = "24"
   }
 
