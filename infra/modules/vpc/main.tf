5c9a1b8e97bf5d0615bef283095977a84b155da9
Vadivel M <vadivel.m@collinsongroup.com>
2026-05-17 23:47:49 +0530

    Fixed Terraform VPC counts and updated provider lock

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -64,7 +64,7 @@ resource "aws_route_table" "public" {
   }
 }
 resource "aws_route_table_association" "public" {
-  count = length(aws_subnet.public)
+  count = length(var.public_subnets)
 
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
@@ -80,7 +80,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(aws_subnet.private)
+  count = length(var.private_subnets)
 
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
